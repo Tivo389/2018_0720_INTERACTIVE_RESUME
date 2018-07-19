252e4712671c4e6b0e1b56bb042d42bd6fb32e50
Shun Miyaki <shunmiyaki@hotmail.com>
2018-07-20 02:58:26 +0900

    End of day. CONTINUE:Implement horizontal scroll with 15-16 dummy slides.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './images/logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main id="App">
+        <section>01</section>
+        <section>02</section>
+        <section>03</section>
+        <section>04</section>
+        <section>05</section>
+        <section>06</section>
+        <section>07</section>
+        <section>08</section>
+        <section>09</section>
+        <section>10</section>
+        <section>11</section>
+        <section>12</section>
+        <section>13</section>
+        <section>14</section>
+        <section>15</section>
+      </main>
     );
   }
 }
