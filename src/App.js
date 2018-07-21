ead4c0099384589e15d01085aa27cb1c6a1153f9
Shun Miyaki <shunmiyaki@hotmail.com>
2018-07-22 00:58:41 +0900

    horizontalScrollTest - Simple scroll converter added. - NEXT: Need to adjust it so will perform one page scroll.

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <main id="App">
-        <section>01</section>
-        <section>02</section>
-        <section>03</section>
-        <section>04</section>
-        <section>05</section>
-        <section>06</section>
-        <section>07</section>
-        <section>08</section>
-        <section>09</section>
-        <section>10</section>
-        <section>11</section>
-        <section>12</section>
-        <section>13</section>
-        <section>14</section>
-        <section>15</section>
+      <main id="app" onWheel={this.scroll}>
+        <section><h1>01</h1></section>
+        <section><h1>02</h1></section>
+        <section><h1>03</h1></section>
+        <section><h1>04</h1></section>
+        <section><h1>05</h1></section>
+        <section><h1>06</h1></section>
+        <section><h1>07</h1></section>
+        <section><h1>08</h1></section>
+        <section><h1>09</h1></section>
+        <section><h1>10</h1></section>
+        <section><h1>11</h1></section>
+        <section><h1>12</h1></section>
+        <section><h1>13</h1></section>
+        <section><h1>14</h1></section>
+        <section><h1>15</h1></section>
       </main>
     );
+  }
+
+  scroll = (e) => {
+    console.log(Math.floor(e.deltaY));
+    e.preventDefault();
+    e.currentTarget.scrollLeft += e.deltaY;
   }
 }
 
