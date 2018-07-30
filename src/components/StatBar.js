2f22d2d9542f04c31f66c60c1c9f4349d0b82f8e
Shun Miyaki <shunmiyaki@hotmail.com>
2018-07-30 21:42:44 +0900

    statBarComponent - Initial setup for statBar Component complete. - Will work on sections, radar graph first, followed by bar graph. - NEXT: Radar Graph. (Ability to animate, data reading will follow.)

--- src/components/StatBar.js
+++ src/components/StatBar.js
@@ -4,9 +4,21 @@ class StatBar extends Component {
 
   // RENDER OF COMPONENT
   render() {
+    const isActive = this.props.statBarActive;
+    const classValue = isActive ? 'active' : '';
     return (
-      <div id="statBar">
+      <div id="statBar" className={ classValue }>
+        <div>
+          {<object
+            data={require('../images/radarGraph.svg')}
+            type="image/svg+xml"
+            alt="Radar graph of skillset">
+          </object>}
 
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     );
   }
