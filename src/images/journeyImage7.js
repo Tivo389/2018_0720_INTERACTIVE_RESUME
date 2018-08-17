6d0469946a097bee362ae544fba62a6a7ee24aca
Shun Miyaki <shunmiyaki@hotmail.com>
2018-08-17 23:47:45 +0900

    animateJoruney7 Complete

--- src/images/journeyImage7.js
+++ src/images/journeyImage7.js
@@ -1,58 +1,45 @@
 export const j7 = `
   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 300 300">
     <defs>
-      <rect id="journeyimage07-a" width="224" height="140" rx="4"/>
-      <rect id="journeyimage07-c" width="224" height="140" rx="4"/>
+      <rect id="journeyimage7-a" width="287" height="179" rx="5.12"/>
+      <rect id="journeyimage7-b" width="287" height="179" rx="5.12"/>
     </defs>
     <g fill="none" fill-rule="evenodd">
-      <g transform="translate(38 80)">
-        <mask id="journeyimage07-b" fill="#fff">
-          <use xlink:href="#journeyimage07-a"/>
-        </mask>
-        <use fill="#E4EEF2" xlink:href="#journeyimage07-a"/>
-        <rect width="224" height="130" y="10" fill="#404040" mask="url(#journeyimage07-b)"/>
-        <circle cx="5" cy="5" r="2" fill="gray" mask="url(#journeyimage07-b)"/>
-        <circle cx="11" cy="5" r="2" fill="gray" mask="url(#journeyimage07-b)"/>
-        <circle cx="17" cy="5" r="2" fill="gray" mask="url(#journeyimage07-b)"/>
-        <g transform="translate(9 19)">
-          <rect width="20" height="4" y="108" fill="#EE86A8"/>
-          <polygon fill="#0093BF" points="4 100 12 100 12 104 4 104"/>
-          <polygon fill="#0093BF" points="8 92 125 92 125 96 8 96"/>
-          <polygon fill="#0093BF" points="4 84 28 84 28 88 4 88"/>
-          <polygon fill="#0093BF" points="8 76 125 76 125 80 8 80"/>
-          <polygon fill="#0093BF" points="4 68 61 68 61 72 4 72"/>
-          <polygon fill="#0093BF" points="4 60 76 60 76 64 4 64"/>
-          <polygon fill="#0093BF" points="4 52 76 52 76 56 4 56"/>
-          <polygon fill="#0093BF" points="4 44 76 44 76 48 4 48"/>
-          <rect width="27" height="4" y="36" fill="#EE86A8"/>
-          <rect width="20" height="4" y="24" fill="#EE86A8"/>
-          <polygon fill="#0093BF" points="4 16 130 16 130 20 4 20"/>
-          <polygon fill="#0093BF" points="4 8 98 8 98 12 4 12"/>
-          <rect width="79" height="4" fill="#EE86A8"/>
-        </g>
-        <mask id="journeyimage07-d" fill="#fff">
-          <use xlink:href="#journeyimage07-c"/>
-        </mask>
-        <use fill="#A9BEC7" xlink:href="#journeyimage07-c"/>
-        <rect width="224" height="120" y="20" fill="#E4EEF2" mask="url(#journeyimage07-d)"/>
-        <rect width="218" height="7" x="3" y="10" fill="#FFF" mask="url(#journeyimage07-d)" rx="2"/>
-        <circle cx="5" cy="5" r="2" fill="gray" mask="url(#journeyimage07-d)"/>
-        <circle cx="11" cy="5" r="2" fill="gray" mask="url(#journeyimage07-d)"/>
-        <circle cx="17" cy="5" r="2" fill="gray" mask="url(#journeyimage07-d)"/>
-        <g mask="url(#journeyimage07-d)">
-          <g transform="translate(27 53)">
-            <rect width="70" height="10" fill="#EE86A8"/>
-            <rect width="50" height="4" y="30" fill="#EE86A8"/>
-            <rect width="50" height="4" y="38" fill="#EE86A8"/>
-            <rect width="50" height="4" y="46" fill="#EE86A8"/>
-            <rect width="80" height="50" x="90" fill="#0093BF"/>
-          </g>
+      <g transform="translate(7 60)">
+        <use fill="#E4EEF2" xlink:href="#journeyimage7-a"/>
+        <path fill="#404040" d="M0,12.7857143 L287,12.7857143 L287,173.88 C287,176.707698 284.707698,179 281.88,179 L5.12,179 C2.29230208,179 4.78718522e-15,176.707698 4.4408921e-15,173.88 L0,12.7857143 Z"/>
+        <ellipse cx="6.406" cy="6.393" fill="gray" rx="2.563" ry="2.557"/>
+        <ellipse cx="14.094" cy="6.393" fill="gray" rx="2.563" ry="2.557"/>
+        <ellipse cx="21.781" cy="6.393" fill="gray" rx="2.563" ry="2.557"/>
+        <g class="code" transform="translate(11.531 24.293)">
+          <rect width="25.625" height="5.114" y="138.086" fill="#EE86A8"/>
+          <polygon fill="#0093BF" points="5.125 127.857 15.375 127.857 15.375 132.971 5.125 132.971"/>
+          <polygon fill="#0093BF" points="10.25 117.629 160.156 117.629 160.156 122.743 10.25 122.743"/>
+          <polygon fill="#0093BF" points="5.125 107.4 35.875 107.4 35.875 112.514 5.125 112.514"/>
+          <polygon fill="#0093BF" points="10.25 97.171 160.156 97.171 160.156 102.286 10.25 102.286"/>
+          <polygon fill="#0093BF" points="5.125 86.943 78.156 86.943 78.156 92.057 5.125 92.057"/>
+          <polygon fill="#0093BF" points="5.125 76.714 97.375 76.714 97.375 81.829 5.125 81.829"/>
+          <polygon fill="#0093BF" points="5.125 66.486 97.375 66.486 97.375 71.6 5.125 71.6"/>
+          <polygon fill="#0093BF" points="5.125 56.257 97.375 56.257 97.375 61.371 5.125 61.371"/>
+          <rect width="34.594" height="5.114" y="46.029" fill="#EE86A8"/>
+          <rect width="25.625" height="5.114" y="30.686" fill="#EE86A8"/>
+          <polygon fill="#0093BF" points="5.125 20.457 166.563 20.457 166.563 25.571 5.125 25.571"/>
+          <polygon fill="#0093BF" points="5.125 10.229 125.563 10.229 125.563 15.343 5.125 15.343"/>
+          <rect width="101.219" height="5.114" fill="#EE86A8"/>
         </g>
       </g>
-      <g transform="translate(38 129)">
-        <circle cx="55" cy="116" r="25" fill="#A9BEC7"/>
-        <path fill="#0093BF" d="M224,91 L224,0 L271.925965,-3.55271368e-15 C275.115129,-1.30600674e-15 278.113035,1.52114774 279.996268,4.09490006 L301.21091,33.0882434 C304.972428,38.2289855 307,44.433594 307,50.8035432 L307,91 L307,111 L0,111 L0,91 L224,91 Z"/>
-        <circle cx="267" cy="116" r="25" fill="#A9BEC7"/>
+      <g class="browser" transform="translate(7 60)">
+        <use fill="#A9BEC7" xlink:href="#journeyimage7-b"/>
+        <path fill="#E4EEF2" d="M0,25.5714286 L287,25.5714286 L287,173.88 C287,176.707698 284.707698,179 281.88,179 L5.12,179 C2.29230208,179 5.67536364e-15,176.707698 5.32907052e-15,173.88 L0,25.5714286 Z"/>
+        <rect width="279.313" height="8.95" x="3.844" y="12.786" fill="#FFF" rx="2.56"/>
+        <ellipse cx="6.406" cy="6.393" fill="gray" rx="2.563" ry="2.557"/>
+        <ellipse cx="14.094" cy="6.393" fill="gray" rx="2.563" ry="2.557"/>
+        <ellipse cx="21.781" cy="6.393" fill="gray" rx="2.563" ry="2.557"/>
+        <rect width="89.688" height="12.786" x="34.594" y="67.764" fill="#EE86A8"/>
+        <rect width="64.063" height="5.114" x="34.594" y="106.121" fill="#EE86A8"/>
+        <rect width="64.063" height="5.114" x="34.594" y="116.35" fill="#EE86A8"/>
+        <rect width="64.063" height="5.114" x="34.594" y="126.579" fill="#EE86A8"/>
+        <rect width="102.5" height="63.929" x="149.906" y="67.764" fill="#0093BF"/>
       </g>
     </g>
   </svg>
